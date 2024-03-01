d267fe7d43a561b2b10ae4eab4ab28e1e58f25f4
Fauzan <hi@fauzanelka.id>
2024-03-01 17:21:13 +0700

    fix(script): gzip and rclone commands

--- scripts/run-backup-daily.sh
+++ scripts/run-backup-daily.sh
@@ -3,7 +3,7 @@
 
 RESULT_FILE="$DAILY_BACKUP_DIR/$MYSQL_DATABASE-$(date +%Y%m%d%H%M%S).sql"
 
-if mysqldump --single-transaction --set-gtid-purged=OFF --skip-add-drop-table --no-tablespaces --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --result-file="$RESULT_FILE" "${MYSQL_DATABASE}" && gzip "$RESULT_FILE" > "$RESULT_FILE.gz"; then
+if mysqldump --single-transaction --set-gtid-purged=OFF --skip-add-drop-table --no-tablespaces --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --result-file="$RESULT_FILE" "${MYSQL_DATABASE}" && gzip "$RESULT_FILE"; then
     echo "$MYSQL_DATABASE dump was successful."
 else
     echo "$MYSQL_DATABASE dump failed."
@@ -12,7 +12,7 @@ fi
 
 find "$DAILY_BACKUP_DIR" -type f -name '*.sql' -mtime +7 -exec rm {} \;
 
-if rclone copy "$RESULT_FILE.gz" "s3:$RCLONE_S3_BUCKET/"; then
+if rclone copy "$RESULT_FILE.gz" ":s3:$RCLONE_S3_BUCKET/"; then
     echo "$RESULT_FILE.gz successfully copied to remote location."
 else
     echo "Failed to copy $RESULT_FILE.gz to remote location."
