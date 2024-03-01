aafb2d096e87ed0bacfbaff863b691373bc7eaf7
Fauzan <hi@fauzanelka.id>
2024-03-01 16:48:37 +0700

    fix(script): $MONTHLY_BACKUP_DIR typo

--- scripts/run-backup-monthly.sh
+++ scripts/run-backup-monthly.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Backup daily with 365 days rotation
 
-RESULT_FILE="$DAILY_BACKUP_DIR/$MYSQL_DATABASE-$(date +%Y%m%d%H%M%S).sql"
+RESULT_FILE="$MONTHLY_BACKUP_DIR/$MYSQL_DATABASE-$(date +%Y%m%d%H%M%S).sql"
 
 if mysqldump --single-transaction --set-gtid-purged=OFF --skip-add-drop-table --no-tablespaces --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --result-file="$RESULT_FILE" "${MYSQL_DATABASE}" && gzip "$RESULT_FILE" > "$RESULT_FILE.gz"; then
     echo "$MYSQL_DATABASE dump was successful."
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-find "$DAILY_BACKUP_DIR" -type f -name '*.sql' -mtime +365 -exec rm {} \;
+find "$MONTHLY_BACKUP_DIR" -type f -name '*.sql' -mtime +365 -exec rm {} \;
 
 if rclone copy "$RESULT_FILE.gz" "s3:$RCLONE_S3_BUCKET/"; then
     echo "$RESULT_FILE.gz successfully copied to remote location."
